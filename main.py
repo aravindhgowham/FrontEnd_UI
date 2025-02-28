3872f0db6271e81d1eb18b69e6c06b19952ce8a9
Dev.support-17 <aravindh.a@valuehealthsol.com>
2025-02-28 13:53:28 +0530

    backend api configuration done

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-import uvicorn
-from fastapi import FastAPI
-from src.llm_utils.pydantic_params.params import UserChatRequest
-from src.llm_utils.llm_engine import Bot_Assistant
-from fastapi.middleware.cors import CORSMiddleware
-import streamlit as st
-
-
-app = FastAPI(title='AI-Literature')
-origins = [
-    "*"
-]
-for i in [app]:
-    i.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    )
-import time
-
-
-
-
 import streamlit as st
 import requests
 
@@ -33,7 +8,7 @@ user_id = "123"  # Replace with actual user ID
 user_input = st.text_input("Your prompt:", "")
 
 if st.button("call me!"):
-    url = "http://localhost:8000/user/chat"
+    url = "https://conversational-ai-production.up.railway.app/user/chat"
     data = {"user_id": user_id, "user_input": user_input}  # Matches FastAPI schema
 
     try:
@@ -49,16 +24,3 @@ if st.button("call me!"):
         st.error("Failed to connect to FastAPI. Make sure the server is running!")
 
 
-@app.post('/user/chat')
-def UserChat(userparam:UserChatRequest):
-    start = time.time()
-    response= Bot_Assistant().prephase_Bot(UserInput=userparam.user_input)
-    print("[+]response = ",response)
-    print("Time taken to load the model: ",time.time()-start)
-    return {'response':response['model_raw_output'],'resposne_time':time.time()-start}
-
-
-
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, workers=2, reload=True)  
